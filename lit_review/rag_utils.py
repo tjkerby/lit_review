6aebd1ef74b09e0a12beb7542ce15f358ec63370
tjkerby <tjkerby@gmail.com>
2025-02-10 18:10:58 +0000

    Got RAG working with Ollama and fixed context window issue.

--- lit_review/rag_utils.py
+++ lit_review/rag_utils.py
@@ -1,5 +1,9 @@
 import torch
 import torch.nn.functional as F
+import time
+import sys
+from langchain_neo4j import Neo4jVector
+from langchain_ollama import OllamaEmbeddings
 
 def compute_embedding(text, tokenizer, model):
     inputs = tokenizer(text, return_tensors="pt")
@@ -24,3 +28,37 @@ def compute_embedding_nomic(text, tokenizer, model, config):
     embedding = F.normalize(embedding, p=2, dim=1)
     embedding_list = embedding.squeeze().tolist()
     return embedding_list
+
+def query_kg(question, llm, kg, config, custom_query, prompt_template, k=30):
+    chunk_vector = Neo4jVector.from_existing_index(
+        OllamaEmbeddings(model=config['embedding']['model_id']),
+        graph=kg, 
+        index_name=config['rag']['index_name'],
+        embedding_node_property=config['rag']['embedding_node_property'],
+        text_node_property=config['rag']['text_node_property'],
+        retrieval_query=custom_query,
+    )
+    question = "search_query: " + question
+    retrieved_docs = chunk_vector.similarity_search_with_score(question, k=k)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _ in retrieved_docs])
+    prompt = prompt_template.format(context=context_text, question=question)
+
+    buffer = ""
+    flush_interval = 0.5  # seconds
+    last_flush = time.time()
+
+
+    for chunk in llm.stream(prompt):
+        buffer += chunk
+        current_time = time.time()
+        # Every flush_interval seconds, output the buffer and reset it.
+        if current_time - last_flush >= flush_interval:
+            sys.stdout.write(buffer)
+            sys.stdout.flush()
+            buffer = ""
+            last_flush = current_time
+
+    if buffer:
+        sys.stdout.write(buffer)
+        sys.stdout.flush()
